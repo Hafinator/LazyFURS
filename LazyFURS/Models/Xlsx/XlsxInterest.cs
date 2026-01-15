ead61cfbcc964c16a98996666edda5323d3ccdbe
LokaFab <support@lokafab.com>
2026-01-15 17:25:09 +0100

    Added new Interest model

---
+++ LazyFURS/Models/Xlsx/XlsxInterest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LazyFURS.Models.Xlsx
+{
+    internal class XlsxInterest
+    {
+        public DateTime Date { get; set; }
+        public decimal AmountUSD { get; set; }
+        public decimal AmountEUR { get; set; }
+    }
+}
